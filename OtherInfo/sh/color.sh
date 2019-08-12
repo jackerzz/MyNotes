6bdfc6bd1189d5122ba441f978559f4c6fecf22b
Zhou Zhikai <zhouzhikai@vidsai.com>
2019-08-12 18:18:42 +0800

    go

---
+++ OtherInfo/sh/color.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for i in {31..37}; do
+	echo -e "\033[$i;40mHello world!\033[0m"
+done
+
+echo "hahahahahah"
+
+
+for i in {41..45}; do 
+	echo -e "\033[$imHello world!\033[0m"
+done
+echo "完成"
---
+++ OtherInfo/sh/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "1: $1"
+shift
+echo "2: $2"
+shift
+echo "3: $3"
+
+
